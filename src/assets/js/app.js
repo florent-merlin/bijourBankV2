084c378a81a68b5fb4d63e13c84eb94282e943d9
Florent Merlin <florentmerlin.pro@gmail.com>
2021-10-21 17:20:28 +0200

    supp js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,88 +6,105 @@ $(document).ready(function () {
   $(document).foundation();
 });
 
-
-/** traitement du formulaire **/
-// *le formulaire envoi les données et les enregistre dans "#operationform" 
-// *si formulaire envoyer ne pas faire de redirection
-// *recuperer les données stoker dans #operationForm puis les afficher
-// dans le 'main'=> class="grid-container"
-
-let form = document.getElementById('operationForm');
-let formdata = document.getElementById('exampleModal1');
-
-addEventListener('operation credit');
-
-
-
-
-// let btSubmit = document.querySelector('btSubmit');
-// let desc = document.querySelector('desc');
-// document.createElement('form');
-console.log(form);
-console.log(formdata);
-
-// form.addEventListener
-// FormDataEvent = document.getElementById('operationForm');
-
-// function (eventLisner) {
-//   return eventoneclic;
-// }
-
-// Event.preventDefault(); /**pour recuperer form;*/
-// form.addEventListener("submit", listener [options]);
-
-
-/////////////-- traitement du solde --////////////////////
-let credit = 0 ;
-let debit = 0 ;
-
-const solde = credit - debit;
-montant = solde;
-
-
-if (montant > 0) {
-  console.log(`vous etes dans le positif : ${montant} euros`);
-} else if (montant < 0) {
-  console.log(`vous etes a decouvert : ${montant}  euros`);
-} else {
-  console.log(`votre solde est de : ${montant}  euros`);
+function Money(montant){
+  return new Intl.NumberFormat('fr-FR', {style: 'currency', currency: 'EUR'}).format(montant);
 }
 
-console.log();
+// recuperer et injecter les données dans le HTML
+function template(operation){
+  const debitOrCredit = operation.credit ? 'credit' : 'debit';
+  const img = operation.credit 
+    ? './assets/images/sac-dargent.png' 
+    : './assets/images/depenses.png';
 
-// id="operationForm"
-// id="operator"
-      // value="credit"
-      // value="debit"
-// id="titre" name="titre"
-// id="desc" name="desc"
-// id="montant" name="montant"
-// 
-// class="btSubmit"
+  return ` 
+  <div class="operation ${debitOrCredit}">
+    <div class="grid-x grid-padding-x align-middle">
+      <div class="cell shrink">
+        <div class="picto">
+          <img src="${img}" alt="images pour le ${debitOrCredit}" />
+        </div>
+      </div>
+      <div class="cell auto">
+        <div>
+          <h2>${operation.title}</h2>
+          <small>${operation.describe}</small>
+        </div>
+      </div>
+      <div class="cell small-3 text-right">
+        <div>
+          <p class="count">${Money(operation.total)}</p>
+          <small>${operation.ratio}%</small>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+}
+
+// fonction pour calculer la "lecture des operations"
+function operationsLengthData(operationsData){
+  if (operationsData.length === 0){
+    operationsArray.innerHTML = '<center>aucune operaion</center>';
+  } else {operationsArray.innerHTML = '';
+    operationsData.foreach((calcul) => {
+      // calcul ratio
+      const total = operation.credit ? totalCredit(operationsArray) : totalDebit(operationsArray);
+      calcul.ratio = ((calcul.total * 100) / total).toFixed(2);
+      operationsArray.innerHTML += template(calcul);
+    })
+  }
+}
+
+// ---- GRAPH -----
+
+// fonction pour creer l'operation -- & -- recuperation des valeurs du template
+function operationsCreate(operator, title, describe, total){
+  return {
+    title: title,
+    describe: describe,
+    total: Number(total), 
+    credit: operator === 'credit',
+    ratio: 0,
+  };
+}
+
+// recuperer et injecter les données ds le header pour definir l'entete
+function setHeader(calculs){
+  const solde = totalCredit(calculs) - totalDebit(calculs);
+  document.getElementById('solde').innerHTML = Money(solde);
+
+  const statuts = document.querySelector('#solde + small');
+  let statut = [];
+  if(solde > 0){
+    statut = ["on est bien"];
+    statuts.setAttribute('class', 'good');
+  } else{
+    statut = ["on est mal"];
+    statuts.setAttribute('class', 'bad');
+  }
+
+  const statutComment = Math.floor(Math.random() * statut.length);
+  statuts.innerHTML = statut [statutComment];
+}
+
+// filtrer les operations pour chaque montants. identifier le credit ou le debit 
+function totalCredit(operationsData){
+  const creditOperator = operationsData.filtrer((calcul) => calcul.credit);
+  return creditOperator.reduce((sum, montant) => sum + Number(montant.total), 0);
+}
+
+function totalDebit(operationsData){
+  const debitOperator = operationsData.filtrer((calcul) => !calcul.credit);
+  return debitOperator.reduce((sum, montant) => sum + Number(montant.total), 0);
+}
+
+// fonction pour menu actif "boutton" du formulaire (action au click) debit/credit
 
 
-//  add.;
 
-//  filtre. (conditions " if{}/else{}/else if() ")
-      // Afficher (section credit) & (section debit)
-      // Afficher le solde
+// fonction afficher btSubmit formulaire ds le main
 
-    
-// Rendre le graphique dynamique.
-      // navigation section ("general" , "credit", "debit")
-  
-  
-      // main
-  //     class ="grid-container"
-  //         class ="operation credit"
-  //             class ="grid-x grid-padding-x align-middle"
-  //                 class ="cell shrink"
-  //                     class ="picto"
-  //                       <img src="./assets/images/sac-dargent.png" alt="credit">
-  //                 class="cell auto"
 
-  //                 class="cell small-3 text-right"
-  //                     class="count"
 
-  //         class = "operation credit"
+// fonction pour la nav barre ---interraction GRAPH  ----
