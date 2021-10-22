6e23c9c96c8314112199be29e5d3a9c51fa8b0b5
Florent Merlin <florentmerlin.pro@gmail.com>
2021-10-22 15:20:33 +0200

    ajout js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   $(document).foundation();
 });
 
-function Money(montant){
+function money(montant){
   return new Intl.NumberFormat('fr-FR', {style: 'currency', currency: 'EUR'}).format(montant);
 }
 
@@ -28,12 +28,12 @@ function template(operation){
       <div class="cell auto">
         <div>
           <h2>${operation.title}</h2>
-          <small>${operation.describe}</small>
+          <small>${operation.description}</small>
         </div>
       </div>
       <div class="cell small-3 text-right">
         <div>
-          <p class="count">${Money(operation.total)}</p>
+          <p class="count">${money(operation.total)}</p>
           <small>${operation.ratio}%</small>
         </div>
       </div>
@@ -45,24 +45,35 @@ function template(operation){
 // fonction pour calculer la "lecture des operations"
 function operationsLengthData(operationsData){
   if (operationsData.length === 0){
-    operationsArray.innerHTML = '<center>aucune operaion</center>';
-  } else {operationsArray.innerHTML = '';
+    operationList.innerHTML = '<center>aucune operation</center>';
+  } else {operationList.innerHTML = '';
     operationsData.foreach((calcul) => {
-      // calcul ratio
-      const total = operation.credit ? totalCredit(operationsArray) : totalDebit(operationsArray);
-      calcul.ratio = ((calcul.total * 100) / total).toFixed(2);
-      operationsArray.innerHTML += template(calcul);
-    })
+        // calcul ratio
+        const total = operation.credit ? totalCredit(operationsData) : totalDebit(operationsData);
+        calcul.ratio = ((calcul.total * 100) / total).toFixed(2);
+        operationList.innerHTML += template(calcul);
+      });
   }
 }
 
 // ---- GRAPH -----
+function allGraph(operationsData){
+  let solde = 0;
+  config.data.labels = [];
+  config.data.datasets[0].data = [];
+  chart.update();
+  operationsData.foreach((calcul) => { 
+    const label = `${calcul.title} (${calcul.credit ? '+' : '-'} ${calcul.total})`; 
+    solde = calcul.credit ? solde + calcul.total : solde - calcul.total;
+    addTemperature(label, solde);
+    });
+}
 
 // fonction pour creer l'operation -- & -- recuperation des valeurs du template
-function operationsCreate(operator, title, describe, total){
+function operationsCreate(operator, title, description, total){
   return {
     title: title,
-    describe: describe,
+    description: description,
     total: Number(total), 
     credit: operator === 'credit',
     ratio: 0,
@@ -72,7 +83,7 @@ function operationsCreate(operator, title, describe, total){
 // recuperer et injecter les données ds le header pour definir l'entete
 function setHeader(calculs){
   const solde = totalCredit(calculs) - totalDebit(calculs);
-  document.getElementById('solde').innerHTML = Money(solde);
+  document.getElementById('solde').innerHTML = money(solde);
 
   const statuts = document.querySelector('#solde + small');
   let statut = [];
@@ -90,21 +101,88 @@ function setHeader(calculs){
 
 // filtrer les operations pour chaque montants. identifier le credit ou le debit 
 function totalCredit(operationsData){
-  const creditOperator = operationsData.filtrer((calcul) => calcul.credit);
+  const creditOperator = operationsData.filter((calcul) => calcul.credit);
   return creditOperator.reduce((sum, montant) => sum + Number(montant.total), 0);
 }
 
 function totalDebit(operationsData){
-  const debitOperator = operationsData.filtrer((calcul) => !calcul.credit);
+  const debitOperator = operationsData.filter((calcul) => !calcul.credit);
   return debitOperator.reduce((sum, montant) => sum + Number(montant.total), 0);
 }
 
-// fonction pour menu actif "boutton" du formulaire (action au click) debit/credit
-
-
+// fonction pour menu actif "boutton" du form (action au click) debit/credit
+function menu(elt){
+  all.removeAttribute('class', 'active');
+  credit.removeAttribute('class', 'active');
+  debit.removeAttribute('class', 'active');
+  elt.setAttribute('class', 'active');
+}
 
 // fonction afficher btSubmit formulaire ds le main
-
-
+function submitForm(operationForm) {
+  operationForm.operator.value = '--';
+  operationForm.titre.value = '';
+  operationForm.desc.value = '';
+  operationForm.montant.value = null; 
+}
 
 // fonction pour la nav barre ---interraction GRAPH  ----
+// 
+const all = document.querySelector('.navHeader a');
+all.addEventListener('click', (e) => {
+  activeMenu(all);
+  operationsLengthData(operationsData);
+  allGraph(operationsData);
+});
+
+// page credit
+const credit = document.querySelector('.navHeader a:nth-child(2)');
+credit.addEventListener('click', (e) => {
+  activeMenu(credit);
+  operationsLengthData(operationsData.filter((calcul) => calcul.credit));
+  allGraph(operationsData);
+});
+
+// page debit
+const debit = document.querySelector('.navHeader a:nth-child(3)');
+debit.addEventListener('click', (e) => {
+  activeMenu(debit);
+  operationsLengthData(operationsData.filter((calcul) => !calcul.credit));
+  allGraph(operationsData);
+});
+
+// formulaire nouvelle operation
+const subForm = document.querySelector('#operationForm button[type=submit]');
+subForm.setAttribute('data-close','');
+subForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  const operationForm = document.getElementById('operationForm');
+
+  //------------- test des données ----------
+  const operator = operationForm.operator.value;
+  const titre = operationForm.titre.value;
+  const desc = operationForm.desc.value;
+  const montant = operationForm.montant.value;
+  
+  const calcul = operationsCreate(operator, titre, desc, montant);
+  operationsData.push(calcul);
+  localStorage.setItem('datas', JSON.stringify(operationsData));
+
+  submitForm(operationsData);
+  setHeader(operationsData);
+  allGraph(operationsData);
+  operationsLengthData(operationsData);
+});
+
+// Affichage de la page
+// data test :
+let operationsData = [];
+if (localStorage.getItem('datas')) {
+  operationsData = JSON.parse(localStorage.getItem('datas'));
+}
+
+//----- Affichage principal ------------- 
+const operationList = document.querySelector('main .grid-container');
+setHeader(operationsData);
+operationsLengthData(operationsData);
+allGraph(operationsData);
